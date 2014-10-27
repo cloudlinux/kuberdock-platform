43ed63c0515deba5d42a37323699f8a180dbd4de
Anatoliy Kurinnyy <akurinnyy@cloudlinux.com>
2014-10-28 01:18:00 +0200

    appcloud script update2

--- script/appcloud.py
+++ script/appcloud.py
@@ -4,38 +4,43 @@ from __future__ import print_function
 import argparse, os, sys, socket, hashlib
 import clcommon
 
-DEBUG = False
+
 msg = {}
 msg['usage'] = 'python {0} -h'.format(sys.argv[0])
 msg['debug'] = '[DEBUG]'
 msg['info'] = '[INFO]'
+msg['error'] = '[ERROR]'
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--add-minion', help='Add new minion', type=str)
 parser.add_argument('--del-minion', help='Delete minion', type=str)
 parser.add_argument('--change-minion-ip', help='Change minion IP', type=str, nargs=2)
+parser.add_argument('-v', '--verbose', help='Increase output verbosity', action='store_true')
 args = parser.parse_args()
 
 
 class MinionAdmin(object):
-    def __init__(self, add_minion, del_minion, change_minion_ip):
+    def __init__(self, add_minion, del_minion, change_minion_ip, verbose):
         self.config_fn = './apiserver' #/etc/kubernetes/apiserver
         self.add_ip = add_minion
         self.del_ip = del_minion
         self.change_ip = change_minion_ip
+        self.DEBUG = verbose
 
     def check_ip(self, ip):
         if ip:
             try:
                 socket.inet_aton(ip)
             except:
-                print('{0} Wrong IP: "{1}". Please enter valid IP address!'.format(msg.get('debug'), ip))
+                print('{0} Wrong IP: "{1}". Please enter valid IP address!'.format(msg.get('error'), ip))
                 ip = None
         return ip
 
     def restart_service(self):
         cmd = 'service kube-apiserver restart'
         print('{0} Restarting service...'.format(msg.get('info')))
+        if self.DEBUG:
+            print('{0} {1}'.format(msg.get('debug'), cmd))
         os.system(cmd)
 
     def read_config(self):
@@ -48,9 +53,9 @@ class MinionAdmin(object):
 
     def change(self):
         c = self.read_config()
-        if DEBUG:
-            print(c['MINION_ADDRESSES'])
-        old_hash_iplist = hashlib.sha1(c['MINION_ADDRESSES'])
+        if self.DEBUG:
+            print('{0} {1}'.format(msg.get('debug'), c['MINION_ADDRESSES']))
+        old_hash_iplist = hashlib.sha1(c['MINION_ADDRESSES']).hexdigest()
         conf_iplist = c['MINION_ADDRESSES'].replace('"', '').split(',')
 
         add_ip = self.check_ip(self.add_ip)
@@ -64,26 +69,26 @@ class MinionAdmin(object):
             if add_ip not in conf_iplist:
                 conf_iplist.append(add_ip)
             else:
-                print('{0} "{1}" already in config'.format(msg.get('debug'), add_ip))
+                print('{0} "{1}" already in config'.format(msg.get('error'), add_ip))
         if del_ip: #del
             if del_ip in conf_iplist:
                 conf_iplist.remove(del_ip)
             else:
-                print('{0} "{1}" not found in config'.format(msg.get('debug'), del_ip))
+                print('{0} "{1}" not found in config'.format(msg.get('error'), del_ip))
         if old_ip and new_ip: #change
             if old_ip in conf_iplist:
                 if new_ip not in conf_iplist:
                     conf_iplist.remove(old_ip)
                     conf_iplist.append(new_ip)
                 else:
-                    print('{0} New IP "{1}" already in config'.format(msg.get('debug'), new_ip))
+                    print('{0} New IP "{1}" already in config'.format(msg.get('error'), new_ip))
             else:
-                print('{0} Old IP "{1}" not in config'.format(msg.get('debug'), old_ip))
+                print('{0} Old IP "{1}" not in config'.format(msg.get('error'), old_ip))
         c['MINION_ADDRESSES'] = '"{0}"'.format(','.join(sorted(conf_iplist)))
-        if DEBUG:
-            print(c['MINION_ADDRESSES'])
-        new_hash_iplist = hashlib.sha1(c['MINION_ADDRESSES'])
-        if old_hash_iplist.hexdigest() != new_hash_iplist.hexdigest():
+        if self.DEBUG:
+            print('{0} {1}'.format(msg.get('debug'), c['MINION_ADDRESSES']))
+        new_hash_iplist = hashlib.sha1(c['MINION_ADDRESSES']).hexdigest()
+        if old_hash_iplist != new_hash_iplist:
             self.write_config(c)
             self.restart_service()
 

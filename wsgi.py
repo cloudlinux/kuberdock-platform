9ea83e169c802f1611e305383e2a7c19e3227518
Stas Django <pythonpowerd@gmail.com>
2015-03-06 19:48:34 +0200

    Fix work with uwsgi. Change flannel backend to UDP due some bug at hosting network
    Change-Id: I6c9e47b3e20aa53662838a33551534907c4136d7

--- wsgi.py
+++ wsgi.py
@@ -10,17 +10,18 @@ from gevent.wsgi import WSGIServer
 from kubedock import frontend, api
 from kubedock.core import listen_kub_events
 
+# This renders install scripts with latest settings at every start
+import make_scripts
+
 application = DispatcherMiddleware(
     frontend.create_app(),
     {'/api': api.create_app()}
 )
+gevent.spawn(listen_kub_events)
 
 if __name__ == "__main__":
-    # This renders install scripts with latest settings at every start
-    import make_scripts
 
     @run_with_reloader
     def run_server():
         http_server = WSGIServer(('', 5000), DebuggedApplication(application))
-        gevent.spawn(listen_kub_events)
         http_server.serve_forever()

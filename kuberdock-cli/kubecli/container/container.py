739dd1c045e14e3e041228fb8779d54178417a1c
Igor Savenko <bliss@cloudlinux.com>
2015-07-22 23:51:58 +0300

    Change postprocess iptables rules
    Change-Id: Ibc99453e37778bf00228e8bf51ab516e6f987f35

--- kuberdock-cli/kubecli/container/container.py
+++ kuberdock-cli/kubecli/container/container.py
@@ -1,7 +1,7 @@
 import base64
-import datetime
+#import datetime
 import json
-import logging
+#import logging
 import operator
 import os
 import pwd
@@ -74,16 +74,22 @@ class KubeCtl(KubeQuery, PrintOut, object):
             service_ip = pod[0].get('podIP')
             if service_ip is None:
                 return
-            params = ['/sbin/iptables', '-C', 'OUTPUT', '-d', service_ip,
-                      '-m', 'owner', '!', '--uid-owner', self.uid, '-j', 'REJECT']
+            params = ['/sbin/iptables', '-t', 'nat', '-C', 'OUTPUT', '-d', service_ip,
+                      '-m', 'owner', '!', '--uid-owner', self.uid, '-j', 'DNAT',
+                      '--to-destination', '233.252.0.254']
             try:
-                subprocess.check_call(params, stdout=open('/dev/null', 'a'), stderr=open('/dev/null', 'a'))
+                subprocess.check_call(params, stdout=open('/dev/null', 'a'),
+                                      stderr=open('/dev/null', 'a'))
             except subprocess.CalledProcessError:
                 params[1] = '-I'
-                subprocess.call(params, stdout=open('/dev/null', 'a'), stderr=open('/dev/null', 'a'))
+                subprocess.call(params, stdout=open('/dev/null', 'a'),
+                                stderr=open('/dev/null', 'a'))
         else:
             existing_ips = [i['podIP'] for i in data if 'podIP' in i]
-            rv = subprocess.Popen(['/sbin/iptables', '-L', 'OUTPUT', '-n', '--line-numbers'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            params = ['/sbin/iptables', '-t', 'nat', '-L', 'OUTPUT', '-n',
+                      '--line-numbers']
+            rv = subprocess.Popen(params, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
             o, e = rv.communicate()
             rules = o.splitlines()
             patt = re.compile(r'!\sowner\sUID\smatch\s(?P<uid>\d+)')
@@ -95,11 +101,15 @@ class KubeCtl(KubeQuery, PrintOut, object):
                 if m.group('uid') != self.uid:
                     continue
                 if fields[5] not in existing_ips:
-                    params = ['/sbin/iptables', '-D', 'OUTPUT', fields[0]]
+                    params = ['/sbin/iptables', '-t', 'nat', '-D', 'OUTPUT',
+                              fields[0]]
                     try:
-                        subprocess.check_call(params, stdout=open('/dev/null', 'a'), stderr=open('/dev/null', 'a'))
+                        subprocess.check_call(params,
+                                              stdout=open('/dev/null', 'a'),
+                                              stderr=open('/dev/null', 'a'))
                     except subprocess.CalledProcessError:
-                        print "Could not delete rule for uid {0} ({1})".format(self.uid, fields[5])
+                        print "Could not delete rule for uid {0} ({1})".format(
+                            self.uid, fields[5])
 
     @staticmethod
     def _transform(data):

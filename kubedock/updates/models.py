ac31f59e7759147057d65243bf8fcbddd84d2dfe
Stas Django <pythonpowerd@gmail.com>
2015-07-24 11:09:15 +0300

    Added checkpoints to updates.
    Change-Id: Icaed69c5e3c2d2ed18e88a74a1e32d9514350b75

--- kubedock/updates/models.py
+++ kubedock/updates/models.py
@@ -7,8 +7,18 @@ class Updates(BaseModelMixin, db.Model):
     fname = db.Column(db.Text, primary_key=True, nullable=False)
     status = db.Column(db.Text, nullable=False)
     log = db.Column(db.Text, nullable=True)
+    last_step = db.Column(db.Integer, default=0, nullable=True)
     start_time = db.Column(db.DateTime, nullable=False)
     end_time = db.Column(db.DateTime, nullable=True)
+
+    @property
+    def checkpoint(self):
+        return self.last_step or 0
+
+    @checkpoint.setter
+    def checkpoint(self, val):
+        self.last_step = val
+        self.save()
 
     def print_log(self, *msg):
         if len(msg) > 0:

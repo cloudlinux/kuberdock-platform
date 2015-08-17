d4fa15cd254a4af3880b018e3bffac0af82126a7
Oleg Bednarskiy <obednarsky@cloudlinux.com>
2015-08-17 08:53:17 -0400

    Add jasmine to Ippool application
    Change-Id: Ia86f8c7e29aad5e194b76c3fddab104d60a2434f

--- kubedock/frontend/ippool.py
+++ kubedock/frontend/ippool.py
@@ -3,16 +3,23 @@ from flask import Blueprint, render_template
 from flask.ext.login import login_required
 
 from ..kapi.ippool import IpAddrPool
+from ..settings import TEST
 
 
-ippool = Blueprint('ippool', __name__)
+ippool = Blueprint('ippool', __name__, url_prefix='/ippool')
 
 
-@ippool.route('/ippool/', strict_slashes=False)
-@ippool.route('/ippool/<path:net>', strict_slashes=False)
+@ippool.route('/', strict_slashes=False)
+@ippool.route('/<path:net>', strict_slashes=False)
 @login_required
 def index(net=None):
     """Returns the index page."""
     networks_collection = IpAddrPool().get(net)
     return render_template(
         'ippool/index.html', networks_collection=json.dumps(networks_collection))
+
+@ippool.route('/test', methods=['GET'])
+def run_tests():
+    if TEST:
+        return render_template('t/ippool_index.html')
+    return "not found", 404
---
+++ kubedock/frontend/static/js/ippool_app/tests/app.js
@@ -0,0 +1,13 @@
+define(['ippool_app/app'], function(data){
+
+    describe("TEST jasmine", function(){
+       var jasmine = true;
+
+       it("jasmine is 'true'", function(){
+           expect(jasmine).toBeTruthy();
+       });
+
+    });
+
+    console.log('all tests loaded');
+});

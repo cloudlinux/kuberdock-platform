9f32d168b6a3e44e57d4ae1facd86bd3b72ab59a
Igor Savenko <bliss@cloudlinux.com>
2015-09-26 16:23:02 +0300

    fixed revision for db upgrade
    Change-Id: I9ca1b5e4f6a3921ffb308da68885f6ef273c96f0

--- kubedock/updates/kdmigrations/versions/18f6b46d5b6c_node_missed_actions.py
+++ kubedock/updates/kdmigrations/versions/18f6b46d5b6c_node_missed_actions.py
@@ -1,14 +1,14 @@
 """node missed actions
 
 Revision ID: 18f6b46d5b6c
-Revises: 30bf03408b5e
+Revises: 38bc6f56f2aa
 Create Date: 2015-09-25 16:18:31.492789
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '18f6b46d5b6c'
-down_revision = '30bf03408b5e'
+down_revision = '38bc6f56f2aa'
 
 from alembic import op
 import sqlalchemy as sa

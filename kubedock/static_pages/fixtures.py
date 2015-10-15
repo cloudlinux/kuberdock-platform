4fb24c12694255927ed272a54371d455581e3efc
wncm <sgruntovsky@cloudlinux.com>
2015-10-15 17:26:14 +0300

    fix pods menu item for TrialUser in fixtures
    Change-Id: I8f7dc9b2230543520eafdaec8e450bd18b03b071

--- kubedock/static_pages/fixtures.py
+++ kubedock/static_pages/fixtures.py
@@ -7,7 +7,7 @@ MENUS = [
         region=Menu.REGION_NAVBAR,
         name='Navbar menu',
         items=[
-            dict(name="Pods", path="/", ordering=0, roles=json.dumps(["User"])),
+            dict(name="Pods", path="/", ordering=0, roles=json.dumps(["User", "TrialUser"])),
             dict(name="Nodes", path="/nodes/", ordering=1,
                  roles=json.dumps(["Admin"])),
             # dict(name="Users", path="/users/", ordering=2,

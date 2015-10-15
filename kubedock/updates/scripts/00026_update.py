7576c67fd54457ae85c7766b7cdad456340d0b0e
Aleksandr Tishin <atishin@cloudlinux.com>
2015-10-15 14:46:05 +0300

    AC-1300: Use upgrade_node for setting FS quotas in 00026_update.py
    Change-Id: I5e0a75d1ce2435fb2c1524fd90cd832e633b2f56

--- kubedock/updates/scripts/00026_update.py
+++ kubedock/updates/scripts/00026_update.py
@@ -1,46 +1,9 @@
-from collections import defaultdict
+from fabric.api import run
 
 from kubedock.billing.models import Kube
-from kubedock.core import db, ssh_connect
+from kubedock.core import db
 from kubedock.kapi.podcollection import PodCollection
 from kubedock.users.models import User
-
-
-def _set_limits():
-    spaces = dict((i, (s, u)) for i, s, u in Kube.query.values(
-        Kube.id, Kube.disk_space, Kube.disk_space_units))
-
-    hosts = defaultdict(list)
-    for user in User.query:
-        for pod in PodCollection(user).get(as_json=False):
-            if 'host' not in pod:
-                continue
-            for container in pod['containers']:
-                if 'containerID' not in container:
-                    continue
-                space, unit = spaces.get(pod['kube_type'], (0, 'GB'))
-                disk_space = space * container['kubes']
-                disk_space_unit = unit[0].lower() if unit else ''
-                if disk_space_unit not in ('', 'k', 'm', 'g', 't'):
-                    disk_space_unit = ''
-                disk_space_str = '{0}{1}'.format(disk_space, disk_space_unit)
-                hosts[pod['host']].append('='.join((container['containerID'],
-                                          disk_space_str)))
-
-    for host, limits in hosts.items():
-        ssh, error = ssh_connect(host)
-        if error:
-            raise IOError(error)
-
-        _, o, e = ssh.exec_command(
-            'python /var/lib/kuberdock/scripts/fslimit.py {0}'.format(
-                ' '.join(limits))
-        )
-        exit_status = o.channel.recv_exit_status()
-        ssh.close()
-        if exit_status > 0:
-            raise IOError('Error fslimit.py with exit status {0}'.format(
-                exit_status))
 
 
 def upgrade(upd, with_testing, *args, **kwargs):
@@ -51,7 +14,6 @@ def upgrade(upd, with_testing, *args, **kwargs):
             kube.disk_space = disk_space if disk_space > 0 else 1
             kube.disk_space_units = 'GB'
     db.session.commit()
-    _set_limits()
 
 
 def downgrade(upd, with_testing, exception, *args, **kwargs):
@@ -61,3 +23,38 @@ def downgrade(upd, with_testing, exception, *args, **kwargs):
             kube.disk_space *= 2 ** 10
             kube.disk_space_units = 'MB'
     db.session.commit()
+
+
+def upgrade_node(upd, with_testing, env, *args, **kwargs):
+    upd.print_log('Apply new limits to nodes...')
+
+    spaces = dict((i, (s, u)) for i, s, u in Kube.query.values(
+        Kube.id, Kube.disk_space, Kube.disk_space_units))
+
+    limits = []
+    for user in User.query:
+        for pod in PodCollection(user).get(as_json=False):
+            if pod.get('host') != env.host_string:
+                continue
+            for container in pod['containers']:
+                container_id = container['containerID']
+                if container_id == container['name']:
+                    continue
+                space, unit = spaces.get(pod['kube_type'], (0, 'GB'))
+                disk_space = space * container['kubes']
+                disk_space_units = unit[0].lower() if unit else ''
+                if disk_space_units not in ('', 'k', 'm', 'g', 't'):
+                    disk_space_units = ''
+                limits.append([container_id, disk_space, disk_space_units])
+
+    if not limits:
+        return
+
+    lim_str = ' '.join(['{0}={1}{2}'.format(c, s, u) for c, s, u in limits])
+    upd.print_log(
+        run('python /var/lib/kuberdock/scripts/fslimit.py {0}'.format(lim_str))
+    )
+
+
+def downgrade_node(upd, with_testing, env, exception, *args, **kwargs):
+    upd.print_log('Sorry, no downgrade_node provided')

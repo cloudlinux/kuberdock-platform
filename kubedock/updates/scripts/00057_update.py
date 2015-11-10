65c095dc95567dac5b0bd2d9b9fa5fad6ca5993b
Aleksandr Tishin <atishin@cloudlinux.com>
2015-11-10 14:30:39 +0000

    Node logging timestamp should contain timezone info
    Change-Id: I872c3061042e3a847ac3c2e5c378e069e4eaae7a

--- kubedock/updates/scripts/00057_update.py
+++ kubedock/updates/scripts/00057_update.py
@@ -1,3 +1,5 @@
+from fabric.api import run
+
 from kubedock.users.models import User
 from kubedock.settings import KUBERDOCK_INTERNAL_USER, MASTER_IP
 from kubedock.kapi.nodes import (
@@ -8,27 +10,52 @@ from kubedock.validation import check_internal_pod_data
 from kubedock.kapi.podcollection import PodCollection
 
 
+CONF = '/etc/rsyslog.d/kuberdock.conf'
+PARAM = '*.* @127.0.0.1:5140'
+TEMPLATE = 'RSYSLOG_ForwardFormat'
+
+
 def upgrade(upd, with_testing, *args, **kwargs):
-    upd.print_log('Update logging pods...')
-
-    ki = User.filter_by(username=KUBERDOCK_INTERNAL_USER).first()
-    for pod in PodCollection(ki).get(as_json=False):
-        if pod['name'] == get_kuberdock_logs_pod_name(pod['node']):
-            PodCollection(ki).delete(pod['id'], force=True)
-            logs_config = get_kuberdock_logs_config(
-                pod['node'],
-                pod['name'],
-                pod['kube_type'],
-                pod['containers'][0]['kubes'],
-                pod['containers'][1]['kubes'],
-                MASTER_IP,
-                ki.get_token(),
-            )
-            check_internal_pod_data(logs_config, user=ki)
-            logs_pod = PodCollection(ki).add(logs_config, skip_check=True)
-
-            PodCollection(ki).update(logs_pod['id'], {'command': 'start'})
+    pass
 
 
 def downgrade(upd, with_testing, exception, *args, **kwargs):
-    upd.print_log('Sorry, no downgrade available.')
+    pass
+
+
+def upgrade_node(upd, with_testing, env, *args, **kwargs):
+    upd.print_log('Change log template in rsyslog configuration...')
+
+    run("sed -i '/^{0}/d; a{0};{1}' {2}".format(PARAM, TEMPLATE, CONF))
+    run('systemctl restart rsyslog')
+
+    upd.print_log('Update logging pod...')
+    ki = User.filter_by(username=KUBERDOCK_INTERNAL_USER).first()
+    pod_name = get_kuberdock_logs_pod_name(env.host_string)
+
+    for pod in PodCollection(ki).get(as_json=False):
+        if pod['name'] == pod_name:
+            break
+    else:
+        return
+
+    PodCollection(ki).delete(pod['id'], force=True)
+    logs_config = get_kuberdock_logs_config(
+        env.host_string,
+        pod_name,
+        pod['kube_type'],
+        pod['containers'][0]['kubes'],
+        pod['containers'][1]['kubes'],
+        MASTER_IP,
+        ki.get_token(),
+    )
+    check_internal_pod_data(logs_config, user=ki)
+    logs_pod = PodCollection(ki).add(logs_config, skip_check=True)
+
+    run('docker pull kuberdock/fluentd:1.4')
+
+    PodCollection(ki).update(logs_pod['id'], {'command': 'start'})
+
+
+def downgrade_node(upd, with_testing, env, exception, *args, **kwargs):
+    upd.print_log('Sorry, no downgrade provided')

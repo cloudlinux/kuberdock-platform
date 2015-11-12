6248614c7e4d5ec0bec60eb6100002c7e42ecdad
Aborilov Pavel <paborilov@cloudlinux.com>
2015-11-12 11:18:34 +0300

    AC-1430: move role edit to update script
    Change-Id: Ia637350e0e1f4747f1626e86896b0fbd977a294d

--- kubedock/updates/kdmigrations/versions/3d2db4a87f86_add_role_internal_column.py
+++ kubedock/updates/kdmigrations/versions/3d2db4a87f86_add_role_internal_column.py
@@ -13,17 +13,10 @@ down_revision = '5049471549ba'
 from alembic import op
 import sqlalchemy as sa
 
-from kubedock.rbac.models import Role
-
-
 def upgrade():
     op.add_column('rbac_role', sa.Column('internal', sa.Boolean(),
                                          nullable=False,
                                          server_default='False'))
-    session = sa.orm.sessionmaker()(bind=op.get_bind())
-    role = session.query(Role).filter(Role.rolename == 'HostingPanel').one()
-    role.internal = True
-    session.commit()
 
 
 def downgrade():

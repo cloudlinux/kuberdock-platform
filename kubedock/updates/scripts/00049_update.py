d6e39793ba0bd124fd98b40721f380c4161d77b6
Aborilov Pavel <paborilov@cloudlinux.com>
2015-11-18 16:55:07 +0300

    fix 00049 update according to changes in 00062
    Change-Id: I993fd9a98c001595d94e8c823b7c44d041add29c

--- kubedock/updates/scripts/00049_update.py
+++ kubedock/updates/scripts/00049_update.py
@@ -1,24 +1,33 @@
 import json
-from kubedock.static_pages.models import MenuItem
+from kubedock.static_pages.models import MenuItem, MenuItemRole
+from kubedock.rbac.models import Role
 
 
 def upgrade(upd, with_testing, *args, **kwargs):
     upd.print_log('Add menus Persistent volumes and Public IPs')
+    user = Role.filter(Role.rolename == "User").one()
+    trial_user = Role.filter(Role.rolename == "TrialUser").one()
     public_ips = MenuItem.create(name="Public IPs", path="/publicIPs/",
-                                 ordering=1, menu_id=1,
-                                 roles=json.dumps(["User", "TrialUser"]))
+                                 ordering=1, menu_id=1)
     public_ips.save()
+    perm = MenuItemRole(role=user, menuitem=public_ips)
+    perm = MenuItemRole(role=trial_user, menuitem=public_ips)
+    perm.save()
     p = MenuItem.create(name="Persistent volumes", path="/persistent-volumes/",
-                        ordering=2, menu_id=1,
-                        roles=json.dumps(["User", "TrialUser"]))
+                        ordering=2, menu_id=1)
     p.save()
+    perm = MenuItemRole(role=user, menuitem=p)
+    perm = MenuItemRole(role=trial_user, menuitem=p)
+    perm.save()
 
 
 def downgrade(upd, with_testing,  exception, *args, **kwargs):
     upd.print_log('Delete menus Persistent volumes and Public IPs')
     ps = MenuItem.filter(MenuItem.name == 'Persistent volumes').first()
     if ps:
+        MenuItemRole.filter(MenuItemRole.menuitem == ps).delete()
         ps.delete()
     pip = MenuItem.filter(MenuItem.name == 'Public IPs').first()
     if pip:
+        MenuItemRole.filter(MenuItemRole.menuitem == pip).delete()
         pip.delete()

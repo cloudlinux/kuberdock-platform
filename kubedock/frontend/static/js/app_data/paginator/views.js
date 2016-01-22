9b95ee676a8b66043cf4076d3c3d193e3e368c04
Oleg Bednarskiy <obednarsky@cloudlinux.com>
2016-01-22 13:55:40 +0200

    AC-1976: KuberDock > Predefined Apps page > Align pagination level;
    Change-Id: Ia7cbbdaf8e0145abf9cc96dc474429d07b532593

--- kubedock/frontend/static/js/app_data/paginator/views.js
+++ kubedock/frontend/static/js/app_data/paginator/views.js
@@ -21,13 +21,12 @@ define(['app_data/app', 'tpl!app_data/paginator/paginator.tpl'], function(App, p
         paginateIt: function(evt){
             evt.stopPropagation();
             var tgt = $(evt.target);
-            if (tgt.hasClass('paginatorFirst')) this.model.get('c').getFirstPage();
-            else if (tgt.hasClass('paginatorPrev')) this.model.get('c').getPreviousPage();
+            if (tgt.hasClass('disabled')) return;
+            if (tgt.hasClass('paginatorPrev')) this.model.get('c').getPreviousPage();
             else if (tgt.hasClass('paginatorNext')) this.model.get('c').getNextPage();
-            else if (tgt.hasClass('paginatorLast')) this.model.get('c').getLastPage();
             this.model.get('v').render();
             this.render();
-        }
+        },
     });
 
     return paginator;

f4c49185db52dd1d43ba6059cb7b78fd30052090
Sergey Gruntovsky <sgruntovsky@cloudlinux.com>
2016-03-11 12:03:31 +0300

    AC-2538: forbid fix-price users to start or change pod directly
    Change-Id: I7bb7a53ff0ee8bce69466b3c0bff4e0ae35de284

--- kubedock/api/podapi.py
+++ kubedock/api/podapi.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 from flask.views import MethodView
 from ..decorators import login_required_or_basic_or_token
 from ..decorators import maintenance_protected
-from ..utils import KubeUtils, register_api
-from ..kapi.podcollection import PodCollection
+from ..utils import KubeUtils, register_api, APIError
+from ..kapi.podcollection import PodCollection, PodNotFound
+from ..pods.models import Pod
+from ..system_settings.models import SystemSettings
 from ..validation import check_new_pod_data, check_change_pod_data
 from ..rbac import check_permission
 
@@ -29,11 +31,37 @@ class PodsAPI(KubeUtils, MethodView):
         return PodCollection(user).add(params)
 
     @maintenance_protected
-    @check_permission('edit', 'pods')
+    # TODO: uncomment and remove hack in the next sprint
+    # @check_permission('edit', 'pods')
     def put(self, pod_id):
         user = self._get_current_user()
         params = self._get_params()
         data = check_change_pod_data(params)
+
+        # yea, it's a dirty-dirty hack, but we don't have a time for smth better
+        # TODO: remove it in the next sprint
+        db_pod = Pod.query.get(pod_id)
+        if db_pod is None:
+            raise PodNotFound()
+        privileged = False
+        if user.is_administrator():
+            user = db_pod.owner
+            privileged = True  # admin interacts with user's pod
+        if (SystemSettings.get_by_name('billing_type').lower() != 'no billing' and
+                user.package.count_type == 'fixed' and not privileged):
+            if params.get('command') == 'set':
+                # fix-price user is not allowed to change paid/unpaid status
+                # and start pod directly, only through billing system
+                raise APIError('Direct requests are forbidden for fixed-price users.', 403)
+            kubes = db_pod.kubes_detailed
+            for container in data.get('containers', []):
+                if container.get('kubes') is not None:
+                    kubes[container['name']] = container['kubes']
+            if params.get('command') == 'redeploy' and db_pod.kubes != sum(kubes.values()):
+                # fix-price user is not allowed to upgrade pod
+                # directly, only through billing system
+                raise APIError('Direct requests are forbidden for fixed-price users.', 403)
+
         pods = PodCollection(user)
         return pods.update(pod_id, data)
     patch = put

62b5944389548644653f5e9163ef854c0a4f7411
Rinat Sabitov <rsabitov@cloudlinux.com>
2016-04-13 10:09:41 +0300

    AC-2907 Prepared test environment
    Change-Id: Icd97ae0d534d108403adfa45c561f0cf3dd899ba

---
+++ dev-utils/prepare_test_env.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+yum install -y gcc
+yum install -y epel-release
+yum install -y libpqxx-devel
+yum install -y python-devel
+yum install -y gmp-devel
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
+pip install --upgrade pip
+pip install virtualenv
+
+virtualenv kuberdock_env
+source kuberdock_env/bin/activate
+pip install -U setuptools
+pip install funcsigs
+pip install pyyaml
+pip install python-dateutil
+pip install nose -I
+cd /vagrant
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+sudo -u postgres psql -c "CREATE DATABASE testkuberdock OWNER kuberdock ENCODING 'UTF8'"
+
+# nosetests -a '!k8s','!docker_registry' /vagrant

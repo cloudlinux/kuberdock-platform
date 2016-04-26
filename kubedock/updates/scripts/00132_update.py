281d157f6677934fbd96b6c69a5207343c694cdc
Aborilov Pavel <paborilov@cloudlinux.com>
2016-04-26 09:48:24 +0300

    AC-3029: stop installing old kernel on nodes
    Remove our patched kernel from nodes and install new one from repo.
    
    Change-Id: I098d85cc2ac866895238b47b8b4eed5ecf37841c

---
+++ kubedock/updates/scripts/00132_update.py
@@ -0,0 +1,38 @@
+from fabric.api import run
+from kubedock.updates.helpers import install_package, reboot_node
+
+old_version = "3.10.0-229.11.1.el7.centos"
+
+
+def upgrade(upd, with_testing, *args, **kwargs):
+    upd.print_log('No upgrade provided for master')
+
+
+def downgrade(upd, with_testing,  exception, *args, **kwargs):
+    upd.print_log('No downgrade provided for master')
+
+
+def upgrade_node(upd, with_testing, *args, **kwargs):
+    yum_base_no_kube = 'yum install --disablerepo=kube -y '
+
+    run(yum_base_no_kube + 'kernel')
+    run(yum_base_no_kube + 'kernel-tools')
+    run(yum_base_no_kube + 'kernel-tools-libs')
+    run(yum_base_no_kube + 'kernel-headers')
+    run(yum_base_no_kube + 'kernel-devel')
+
+    run('rpm -e -v --nodeps kernel-' + old_version)
+    run('yum remove -y kernel-tools-' + old_version)
+    run('yum remove -y kernel-tools-libs-' + old_version)
+    run('yum remove -y kernel-headers-' + old_version)
+    run('yum remove -y kernel-devel-' + old_version)
+    reboot_node(upd)
+
+
+def downgrade_node(upd, with_testing,  exception, *args, **kwargs):
+    install_package('kernel-'+old_version, action='upgrade')
+    install_package('kernel-tools-'+old_version, action='upgrade')
+    install_package('kernel-tools-libs-'+old_version, action='upgrade')
+    install_package('kernel-headers-'+old_version, action='upgrade')
+    install_package('kernel-devel-'+old_version, action='upgrade')
+    reboot_node(upd)

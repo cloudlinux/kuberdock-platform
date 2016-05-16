bf2f449cd4757d9e9befc9c8bfa527458c84f93c
Aborilov Pavel <paborilov@cloudlinux.com>
2016-05-16 17:42:24 +0300

    AC-3184: change columns type to text
    Change columns type from String(255) to Text:
    notification_roles.target
    notifications.message
    
    Change-Id: I36d7ec976c690b5a48b59a707b2de07c1c89be60

---
+++ kubedock/updates/kdmigrations/versions/3c832810a33c_change_type_to_text.py
@@ -0,0 +1,24 @@
+"""change type to text
+
+Revision ID: 3c832810a33c
+Revises: 45e4b1e232ad
+Create Date: 2016-05-16 09:55:57.788558
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3c832810a33c'
+down_revision = '45e4b1e232ad'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('notification_roles', 'target', type_=sa.Text)
+    op.alter_column('notifications', 'message', type_=sa.Text)
+
+
+def downgrade():
+    op.alter_column('notification_roles', 'target', type_=sa.String(255))
+    op.alter_column('notifications', 'message', type_=sa.String(255))

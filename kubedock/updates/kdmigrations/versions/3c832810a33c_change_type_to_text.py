e5f805080d4c6141ccc85667cf19886722963d2c
Igor Savenko <bliss@cloudlinux.com>
2016-05-21 22:27:12 +0300

    Change downgrade revision
    Change-Id: I0a852235042769e09bc026c2b9e9e6a770fa0361

--- kubedock/updates/kdmigrations/versions/3c832810a33c_change_type_to_text.py
+++ kubedock/updates/kdmigrations/versions/3c832810a33c_change_type_to_text.py
@@ -8,7 +8,7 @@ Create Date: 2016-05-16 09:55:57.788558
 
 # revision identifiers, used by Alembic.
 revision = '3c832810a33c'
-down_revision = '45e4b1e232ad'
+down_revision = '220dacf65cba'
 
 from alembic import op
 import sqlalchemy as sa

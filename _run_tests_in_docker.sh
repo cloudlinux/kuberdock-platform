cd09ee8432e3188f20a62cd4eb84b7c06b6a1d60
Max Lobur <mlobur@cloudlinux.com>
2016-06-01 23:16:18 +0300

    Speed up docker unit tests.
    New time 1.34 VS old time 2.50
    (Where sleep - will be fixed soon - is 30s, and test run is 56.924s)
    
    Also remove tox-based py27 local env - noone really uses it.
    Rename 'docker' to 'unit' as this is the only way to run unit tests
    via tox.
    
    Change-Id: I37244b0951730086535e68ecd5e81b34296d0f3f

--- _run_tests_in_docker.sh
+++ _run_tests_in_docker.sh
@@ -3,14 +3,15 @@ build_number=${BUILD_NUMBER:-local}
 project="appcloudunittest${build_number}"
 compose="docker-compose -f unittest-compose.yml -p ${project}"
 
-#$compose build
-echo "##################### Running unit tests ###########################"
 $compose run --rm appcloud /bin/bash -c \
 "echo 'Sleep 30 sec - wait other services. FIXME in AC-3267';
  sleep 30;
- echo 'Run unit tests';
- tox -epy27"
+ echo '###################### Setup requirements ######################';
+ source /venv/bin/activate;
+ pip install -r requirements.txt -r requirements-dev.txt;
+ echo '######################## Run unit tests ########################';
+ nosetests -v kubedock kuberdock-cli"
 ret=$?
-echo "##################### Finished unit tests ###########################"
+
 $compose down --rmi local -v
 exit $ret

d90ad79d6b89dfdcd8ed2dda80051d146787fe58
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2016-06-02 10:41:59 +0300

    AC-3267 Change sleep to port wait with timeout
    Change-Id: Ief573d459ec74a3dcfe782f939c2230922d1fe89

--- _run_tests_in_docker.sh
+++ _run_tests_in_docker.sh
@@ -4,8 +4,9 @@ project="appcloudunittest${build_number}"
 compose="docker-compose -f unittest-compose.yml -p ${project}"
 
 $compose run --rm appcloud /bin/bash -c \
-"echo 'Sleep 30 sec - wait other services. FIXME in AC-3267';
- sleep 30;
+"set -e;
+ echo 'Waiting for postgres 5432 port';
+ timeout 30 bash -c 'until nmap --open -p5432 postgres | grep open; do echo \"Waiting..\"; sleep 1; done;'
  echo '###################### Setup requirements ######################';
  source /venv/bin/activate;
  pip install -r requirements.txt -r requirements-dev.txt;

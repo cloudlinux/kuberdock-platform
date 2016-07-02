d5ed7227c72ee4af29ddba138ae04e2b8810c1c8
Max Lobur <mlobur@cloudlinux.com>
2016-07-02 18:34:49 +0300

    AC-3746: Fix 149 update - skip it if no nodes found on the cluster
    Change-Id: I3422228f074ef80ec143e8c7e6ec6756837e1a96

--- kubedock/updates/scripts/00149_update.py
+++ kubedock/updates/scripts/00149_update.py
@@ -5,6 +5,7 @@ from kubedock.settings import KUBERDOCK_INTERNAL_USER
 from kubedock.users.models import User
 from kubedock.utils import POD_STATUSES
 from kubedock.validation import check_internal_pod_data
+from kubedock.nodes.models import Node
 
 # NOTE(lobur): this script cannot be merged into 148 (k8s & etcd upgrade)
 # because it requires alive cluster to complete. If you put this to 148
@@ -13,10 +14,19 @@ from kubedock.validation import check_internal_pod_data
 
 
 def _recreate_dns_pod(upd, dns_pod_config):
+    upd.print_log("Deleting current DNS pod.")
     user = User.filter_by(username=KUBERDOCK_INTERNAL_USER).one()
     dns_pod = Pod.filter_by(name='kuberdock-dns', owner=user).first()
     if dns_pod:
         PodCollection(user).delete(dns_pod.id, force=True)
+
+    # Since usual upgrade is done with healthcheck we can assume all nodes are
+    # in running state.
+    nodes = Node.query.all()
+    if not nodes:
+        upd.print_log("No nodes found on the cluster. The new DNS pod will be"
+                      "added once the 1st node is added to the cluster.")
+        return
 
     check_internal_pod_data(dns_pod_config, user)
     dns_pod = PodCollection(user).add(dns_pod_config, skip_check=True)

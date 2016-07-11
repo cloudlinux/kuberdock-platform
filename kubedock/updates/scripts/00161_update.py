e1dec6072729238611cd98a5a10c6948594a25a8
Aborilov Pavel <paborilov@cloudlinux.com>
2016-07-11 08:42:16 +0300

    AC-3650: wrap with try-except block
    Change-Id: I4ade5cb4079613039ef8ea04b018d0a4d8a0efd2

--- kubedock/updates/scripts/00161_update.py
+++
@@ -1,9 +0,0 @@
-from kubedock.updates import helpers
-
-def upgrade(upd, with_testing, *args, **kwargs):
-    upd.print_log('Upgrading DB...')
-    helpers.upgrade_db(revision='12963e26b673')
-
-def downgrade(upd, with_testing, exception, *args, **kwargs):
-    upd.print_log('Downgrade DB...')
-    helpers.downgrade_db(revision='1a24688cc541')

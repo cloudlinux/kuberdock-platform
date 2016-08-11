a902e2b5a75e6221971affbc64278a95b4eba842
Sergey Gruntovsky <sgruntovsky@cloudlinux.com>
2016-08-11 08:50:41 +0300

    added review groups; corrected .eslintrc
    Change-Id: I5bedbc82b00dda729b63341c44e78c65db81586f

--- kubedock/frontend/static/run-eslint.js
+++ kubedock/frontend/static/run-eslint.js
@@ -1,7 +1,7 @@
 /* eslint-env node, es6 */
 /* eslint no-console: off */
 
-var ERRORS_TRESHOLD = 260;
+var ERRORS_TRESHOLD = 334;
 
 var CLIEngine = require('eslint').CLIEngine;
 var process = require('process');

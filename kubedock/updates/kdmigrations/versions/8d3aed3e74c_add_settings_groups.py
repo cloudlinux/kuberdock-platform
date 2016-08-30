2e66e7c1db7a4105a7110139c2f26141383c37e1
Aleksandr Skorodumov <askorodumov@cloudlinux.com>
2016-08-30 22:22:14 +0300

    AC-4329 Fix 167 update script squashing 167, 175, 177, 182 scripts
    Change-Id: I4d411a1aee1eff1159b88eec691dd2894eca513e

--- kubedock/updates/kdmigrations/versions/8d3aed3e74c_add_settings_groups.py
+++ kubedock/updates/kdmigrations/versions/8d3aed3e74c_add_settings_groups.py
@@ -1,21 +1,26 @@
 """add_settings_groups
 
 Revision ID: 8d3aed3e74c
-Revises: 370f6c5fafff
+Revises: 12963e26b673
 Create Date: 2016-08-25 18:12:06.124915
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '8d3aed3e74c'
-down_revision = '370f6c5fafff'
+down_revision = '12963e26b673'
 
-
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
+
 
 def upgrade():
     conn = op.get_bind()
+    op.drop_column('predefined_apps', 'user_id')
+    op.add_column('pods', sa.Column(
+        'template_plan_name', sa.String(24), nullable=True))
+    op.create_unique_constraint('resource_role_name_unique', 'rbac_permission',
+                                ['resource_id', 'role_id', 'name'])
     op.add_column(
         'system_settings', sa.Column('setting_group', sa.Text, default=''))
     conn.execute(
@@ -52,8 +57,18 @@ def upgrade():
     conn.execute(
         "UPDATE system_settings SET "
         "placeholder='remote access key from cPanel'  WHERE id= 13")
+
+
 def downgrade():
     conn = op.get_bind()
+    op.add_column('predefined_apps', sa.Column(
+        'user_id',
+        sa.Integer,
+        sa.ForeignKey('users.id'),
+        nullable=False,
+        server_default='1'))
+    op.drop_column('pods', 'template_plan_name')
+    op.drop_constraint('resource_role_name_unique', 'rbac_permission')
     op.drop_column('system_settings', 'setting_group')
     conn.execute("UPDATE system_settings SET options='' where id= 10")
     conn.execute("UPDATE system_settings SET value=''  WHERE id= 10")

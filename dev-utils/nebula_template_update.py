d3be062767c235fc17a377a844cb9d51d6365e91
PopovaAnastasiia <apopova@cloudlinux.com>
2016-08-31 21:42:18 +0300

    AC-4331, AC-4333 Bug fix, driver issue in KD template update script
    In case, when driver is not obtainable in the template.
    
    Change-Id: I6c4b79454094ff0e2e5a0f0bf36e85fefd17766a

--- dev-utils/nebula_template_update.py
+++ dev-utils/nebula_template_update.py
@@ -9,23 +9,21 @@ from time import sleep, time
 import xml.etree.ElementTree as ET
 from sys import platform as _platform
 
-
 VM_ACTIVE = 3
 VM_STOPPED = 4
 IMAGE_READY = 1
 
-
 msg_format = '%(asctime)s - %(message)s'
 logging.basicConfig(
-                    level=logging.DEBUG,
-                    format=msg_format,
-                    filename='update_template.log',
-                    filemode='w')
+    level=logging.DEBUG,
+    format=msg_format,
+    filename='update_template.log',
+    filemode='w')
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 formatter = logging.Formatter(msg_format)
 console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
+logging.getLogger().addHandler(console)
 
 
 class PublicPortWaitTimeoutException(Exception):
@@ -63,26 +61,30 @@ class NebulaVMTemplate(object):
             new_name = re.sub(pattern, version, name)
         else:
             new_name = name + ' v2'
-        return new_name
+        if new_name in [image.name for image in self.image_pool]:
+            return self.increase_version(new_name)
+        else:
+            return new_name
 
     def debug_msg(self, msg):
         logging.debug("{0} {1} {0}".format('*' * 10, msg))
 
-    def edit_disk(self, **kwargs):
+    def edit_disk(self, driver=None, image=None, image_uname=None, size=None):
         """
         Change disk settings in template
         """
         root = ET.Element('VMTEMPLATE')
         disk = ET.SubElement(root, 'DISK')
-        driver = ET.SubElement(disk, 'DRIVER')
-        driver.text = kwargs.get('driver', 'raw')
-        image = ET.SubElement(disk, 'IMAGE')
-        image.text = kwargs.get('image')
-        image_uname = ET.SubElement(disk, 'IMAGE_UNAME')
-        image_uname.text = kwargs.get('image_uname')
-        if kwargs.get('size'):
-            size = ET.SubElement(disk, 'SIZE')
-            size.text = kwargs.get('size')
+        if driver:
+            drv = ET.SubElement(disk, 'DRIVER')
+            drv.text = driver
+        img = ET.SubElement(disk, 'IMAGE')
+        img.text = image
+        img_uname = ET.SubElement(disk, 'IMAGE_UNAME')
+        img_uname.text = image_uname
+        if size:
+            sz = ET.SubElement(disk, 'SIZE')
+            sz.text = size
         return ET.tostring(root)
 
     def wait_for_state(self, element, state):
@@ -90,19 +92,20 @@ class NebulaVMTemplate(object):
         end = time() + timeout
         logging.info(
             'Waiting for status of {} with name {}'.format(
-                                        element.ELEMENT_NAME,
-                                        element.name
-                                        ))
+                element.ELEMENT_NAME,
+                element.name
+            ))
         while element.state != state:
             sleep(4)
             element.info()
             if time() > end:
                 raise RuntimeError(
-                      'Timeout for status {} of element {} exceeded'.format(
-                       state, element.name))
+                    'Timeout for status {} of element {} exceeded'.format(
+                        state, element.name))
 
     def wait_net_port(self, ip, port, timeout, try_interval=2):
-        logging.info("Waiting for {0}:{1} to become available.".format(ip, port))
+        logging.info(
+            "Waiting for {0}:{1} to become available.".format(ip, port))
         end = time() + timeout
         while time() < end:
             try:
@@ -129,7 +132,7 @@ class NebulaVMTemplate(object):
         """
         vm_name = "template_{}_upgrade".format(self.template_id)
         logging.info('Bringing up temporary VM from {}'.format(
-                                                        template.name))
+            template.name))
         template.instantiate(name=vm_name)
         self.vm_pool.info(filter=-2, range_start=-1, range_end=-1)
         vm = self.vm_pool.get_by_name(vm_name)
@@ -149,7 +152,7 @@ class NebulaVMTemplate(object):
         vm.info()
         logging.info('Stopping VM with id {}'.format(vm.id))
         self.wait_for_state(vm, VM_STOPPED)
-        vm.delete()
+        vm.finalize()
         logging.info('VM with id {} stopped and deleted'.format(vm.id))
 
     def clone_template(self):
@@ -164,7 +167,7 @@ class NebulaVMTemplate(object):
         logging.debug(ET.tostring(template.xml))
         logging.debug('*' * 30)
         image = self.image_pool.get_by_name(
-                template.xml.find('TEMPLATE/DISK/IMAGE').text)
+            template.xml.find('TEMPLATE/DISK/IMAGE').text)
         image.info()
         self.debug_msg('Info dump for {}'.format(image.name))
         logging.debug(ET.tostring(image.xml))
@@ -174,21 +177,19 @@ class NebulaVMTemplate(object):
         logging.info('Cloning template and image.')
         target_image_name = self.increase_version(image.name)
         image.clone(name=target_image_name)
-        target_template_name = self.increase_version(template.name)
-        template.clone(name=target_template_name)
         self.image_pool.info(filter=-2, range_start=-1, range_end=-1)
         self.debug_msg('Info dump for {}'.format(self.image_pool.pool_name))
         logging.debug(ET.tostring(self.image_pool.xml))
         logging.debug('*' * 30)
+        logging.info('Cloned image to : {}'.format(target_image_name))
         cloned_image = self.image_pool.get_by_name(target_image_name)
         cloned_image.info()
-        logging.info('Cloned image to : {}'.format(target_image_name))
-
-        # wait for image to be copied in datastore
         self.wait_for_state(cloned_image, IMAGE_READY)
         cloned_image.set_persistent()
         cloned_image.info()
 
+        target_template_name = self.increase_version(template.name)
+        template.clone(name=target_template_name)
         self.template_pool.info(filter=-2, range_start=-1, range_end=-1)
         cloned_template = self.template_pool.get_by_name(target_template_name)
         cloned_template.info()
@@ -198,25 +199,30 @@ class NebulaVMTemplate(object):
 
     def run_template_updates(self):
         logging.info(
-                    '*' * 5 +
-                    'Initializing updates for template with id {}'.format(
-                                                        self.template_id) +
-                    '*' * 5)
+            '*' * 5 +
+            'Initializing updates for template with id {}'.format(
+                self.template_id) +
+            '*' * 5)
         template, image = self.clone_template()
 
         # change base image for template
 
         # Saving size to set it back after updates
         template_size = template.xml.find('TEMPLATE/DISK/SIZE')
+        tmp_driver = template.xml.find('TEMPLATE/DISK/DRIVER')
+        template_driver = None
+        if tmp_driver is not None:
+            template_driver = tmp_driver.text
+
         logging.info(
-                    'Setting {} base image to be {}'.format(
-                                                template.name,
-                                                image.name))
+            'Setting {} base image to be {}'.format(
+                template.name,
+                image.name))
         xml_to_update = self.edit_disk(
-                        driver=template.xml.find(
-                               'TEMPLATE/DISK/DRIVER').text,
-                        image=image.name,
-                        image_uname=image.uname)
+            driver=template_driver,
+            image=image.name,
+            image_uname=image.uname)
+        logging.info('xml_to_update:\n {}'.format(xml_to_update))
         template.update(xml_to_update, update_type=1)
         template.info()
 
@@ -229,17 +235,18 @@ class NebulaVMTemplate(object):
         logging.info('Running updates on host: {}'.format(HOST))
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.load_system_host_keys()
 
-        self.wait_net_port(HOST, port=22, timeout=60*5)
+        self.wait_net_port(HOST, port=22, timeout=60 * 5)
+        logging.info('Connecting using {} key-file'.format(
+            self.ssh_key_path))
+        pk = paramiko.RSAKey.from_private_key_file(self.ssh_key_path)
         client.connect(
-                    HOST,
-                    username="root",
-                    key_filename=self.ssh_key_path)
+            hostname=HOST,
+            username="root",
+            pkey=pk)
 
         # Execute command and get its output
         __, stdout, stderr = client.exec_command('yum upgrade -y')
-        logging.info('*' * 30)
         while True:
             while stdout.channel.recv_ready():
                 logging.info(stdout.channel.recv(1024))
@@ -247,8 +254,15 @@ class NebulaVMTemplate(object):
                 logging.info(stdout.channel.recv_stderr(1024))
             if stdout.channel.exit_status_ready():
                 break
-        logging.info('*' * 30)
+                logging.info('*' * 30)
+        _, stdout, stderr = client.exec_command('sync')
+        _, stdout, stderr = client.exec_command("reboot &")
+        sleep(5)
+        logging.info('Reboot')
+        self.wait_net_port(HOST, port=22, timeout=60 * 5)
 
+        client.close()
+        sleep(5)
         self.delete_vm(vm)
 
         image.set_nonpersistent()
@@ -257,42 +271,49 @@ class NebulaVMTemplate(object):
         # Setting size back
         if template_size is not None:
             xml_to_update = self.edit_disk(
-                        driver=template.xml.find(
-                               'TEMPLATE/DISK/DRIVER').text,
-                        image=image.name,
-                        image_uname=image.uname,
-                        size=template_size.text)
-        template.update(xml_to_update, update_type=1)
+                driver=template_driver,
+                image=image.name,
+                image_uname=image.uname,
+                size=template_size.text)
+            logging.info('xml_to_update:\n {}'.format(xml_to_update))
+            template.update(xml_to_update, update_type=1)
         template.info()
+        #              U  M  A
+        permissions = [1, 1, 0,  # owner
+                       1, 1, 0,  # group
+                       1, 1, 0]  # other
+
+        self.client.call("template.chmod", template.id, *permissions)
+        self.client.call("image.chmod", image.id, *permissions)
+
         logging.info(
-                    '*' * 5 +
-                    'Updated template: id - {}, name - {}'.format(
-                                            template.id, template.name) +
-                    '*' * 5)
+            '*' * 5 +
+            'Updated template: id - {}, name - {}'.format(
+                template.id, template.name) +
+            '*' * 5)
 
 
 def update(template_id):
     vm_template = NebulaVMTemplate.factory(
-                     os.environ.get(
-                            'KD_ONE_URL',
-                            'https://some.nebula.host.com:2633/RPC2'),
-                     os.environ['KD_ONE_USERNAME'],
-                     os.environ['KD_ONE_PASSWORD'],
-                     os.environ.get(
-                            'KD_ONE_PRIVATE_KEY',
-                            os.path.expanduser("~/.ssh/id_rsa")),
-                     template_id)
+        os.environ.get(
+            'KD_ONE_URL',
+            'https://some.nebula.host.com:2633/RPC2'),
+        os.environ['KD_ONE_USERNAME'],
+        os.environ['KD_ONE_PASSWORD'],
+        os.environ.get(
+            'KD_ONE_PRIVATE_KEY',
+            os.path.expanduser("~/.ssh/id_rsa")),
+        template_id)
     vm_template.run_template_updates()
 
 
 parser = argparse.ArgumentParser(
-                    description="Updater for OpenNebula template. Usage: \n"
-                                "python nebula_template_update.py <templ_id>")
+    description="Updater for OpenNebula template. Usage: \n"
+                "python nebula_template_update.py <templ_id>")
 parser.add_argument(
-                    "template_id",
-                    help="The id of the template to be updated",
-                    type=int)
-
+    "template_id",
+    help="The id of the template to be updated",
+    type=int)
 
 if __name__ == "__main__":
     args = parser.parse_args()

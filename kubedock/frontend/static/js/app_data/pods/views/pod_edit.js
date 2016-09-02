cb9e63668121ab346956ebf5b01a74e1a7f0f553
Sergey Gruntovsky <sgruntovsky@cloudlinux.com>
2016-09-02 09:51:14 +0300

    AC-4108: render after reset
    Change-Id: I883d0103f95f0cbc3e9fcef255f66858c3d77598

--- kubedock/frontend/static/js/app_data/pods/views/pod_edit.js
+++ kubedock/frontend/static/js/app_data/pods/views/pod_edit.js
@@ -1054,6 +1054,7 @@ define(['app_data/app', 'app_data/model', 'app_data/utils',
 
         resetFielsdsValue: function(){
             this.model.set('env', _.map(this.model.originalImage.get('env'), _.clone));
+            this.render();
         },
     });
 

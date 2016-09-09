5b147d8cec33358662d331aa0e179f37c24dc996
Stas Django <pythonpowerd@gmail.com>
2016-09-09 13:14:43 +0300

    Backported fixes for 163 update from master to 1.4.0 branch
    Change-Id: I63a4f176902c084c4465389acfa16472a559e3f3

--- kubedock/updates/scripts/00163_update.py
+++ kubedock/updates/scripts/00163_update.py
@@ -1,5 +1,6 @@
 import os
 from kubedock.updates import helpers
+from fabric.api import run, put, lcd, local
 from kubedock.settings import MASTER_IP
 
 HOSTNAME = os.environ.get("HOSTNAME")
@@ -12,28 +13,30 @@ K8S_CA_CERT=KUBERNETES_CERTS_DIR + '/ca.crt'
 def upgrade(upd, with_testing, *args, **kwargs):
     # upd.print_log("Generating key for service account")
     sans="IP:{0},IP:10.254.0.1,DNS:kubernetes,DNS:kubernetes.default,DNS:kubernetes.default.svc,DNS:$(hostname)".format(MASTER_IP)
-    helpers.local("""
-cd `mktemp -d`
-curl -L -O --connect-timeout 20 --retry 6 --retry-delay 2 https://storage.googleapis.com/kubernetes-release/easy-rsa/easy-rsa.tar.gz
-tar xzf easy-rsa.tar.gz
-cd easy-rsa-master/easyrsa3
-./easyrsa init-pki
-./easyrsa --batch "--req-cn={primary_cn}@$(date +%s)" build-ca nopass
-./easyrsa --subject-alt-name="{sans}" build-server-full "$(hostname)" nopass
+    tempdir = local('mktemp -d', capture=True)
+    with lcd(tempdir):
+        local('curl -k -L -O --connect-timeout 20 --retry 6 --retry-delay 2 '
+              'https://storage.googleapis.com/kubernetes-release/easy-rsa/'
+              'easy-rsa.tar.gz')
+        local('tar xzf easy-rsa.tar.gz')
+    with lcd(os.path.join(tempdir, 'easy-rsa-master/easyrsa3')):
+        local('./easyrsa init-pki')
+        local('./easyrsa --batch "--req-cn={primary_cn}@$(date +%s)" '
+                'build-ca nopass'.format(primary_cn=MASTER_IP))
+        local('./easyrsa --subject-alt-name="{sans}" build-server-full '
+                '"$(hostname)" nopass'.format(sans=sans))
 
-mkdir -p {certs_dir}
-mv ./pki/ca.crt {certs_dir}/
-mv ./pki/issued/* {certs_dir}/
-mv ./pki/private/* {certs_dir}/
-chown -R kube:kube {certs_dir}
-chmod -R 0440 {certs_dir}/*
-
-    """.format(
-            primary_cn=MASTER_IP,
-            sans=sans,
-            certs_dir=KUBERNETES_CERTS_DIR
-        ))
-
+        local('mkdir -p {certs_dir}'.format(certs_dir=KUBERNETES_CERTS_DIR))
+        local('mv ./pki/ca.crt {certs_dir}/'
+              .format(certs_dir=KUBERNETES_CERTS_DIR))
+        local('mv ./pki/issued/* {certs_dir}/'
+              .format(certs_dir=KUBERNETES_CERTS_DIR))
+        local('mv ./pki/private/* {certs_dir}/'
+              .format(certs_dir=KUBERNETES_CERTS_DIR))
+        local('chown -R kube:kube {certs_dir}'
+              .format(certs_dir=KUBERNETES_CERTS_DIR))
+        local('chmod -R 0440 {certs_dir}/*'
+              .format(certs_dir=KUBERNETES_CERTS_DIR))
 
     # upd.print_log("Updating apiserver config")
     helpers.update_local_config_file(

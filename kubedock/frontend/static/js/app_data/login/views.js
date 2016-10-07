c990c9ffcb9317535987c66ad1aa2b352e83954c
Oleg Bednarskiy <obednarsky@cloudlinux.com>
2016-10-07 16:12:39 +0300

    Upgrade login view to ES6
    Change-Id: Ic87f5656728c9278b4c99072a2235d5f576ce44d

--- kubedock/frontend/static/js/app_data/login/views.js
+++ kubedock/frontend/static/js/app_data/login/views.js
@@ -1,63 +1,56 @@
-define(['app_data/app', 'app_data/model', 'app_data/utils', 'marionette',
-        'app_data/login/templates/login.tpl'],
-    function(App, Model, utils, Marionette, loginTpl){
-        'use strict';
-        var views = {};
+import Model from 'app_data/model';
+import * as utils from 'app_data/utils';
+import loginTpl from 'app_data/login/templates/login.tpl';
 
-        views.LoginView = Marionette.ItemView.extend({
-            template: loginTpl,
-            className: "login-page clearfix",
+export const LoginView = Marionette.ItemView.extend({
+    template: loginTpl,
+    className: "login-page clearfix",
 
-            ui: {
-                'login': '.login',
-                'username': '#login-form-username-field',
-                'password': '#login-form-password-field',
-            },
+    ui: {
+        'login': '.login',
+        'username': '#login-form-username-field',
+        'password': '#login-form-password-field',
+    },
 
-            events: {
-                'click @ui.login': 'signIn',
-                'keypress @ui.username': 'signIn',
-                'keypress @ui.password': 'signIn',
-            },
+    events: {
+        'click @ui.login': 'signIn',
+        'keypress @ui.username': 'signIn',
+        'keypress @ui.password': 'signIn',
+    },
 
-            initialize: function(options){ this.next = options.next || ''; },
-            signIn: function(evt){
-                evt.stopPropagation();
-                if (evt.type === 'keypress' && evt.which !== 13) return;
-                var auth = {
-                    username: this.ui.username.val().trim(),
-                    password: this.ui.password.val().trim(),
-                };
+    initialize: function(options){ this.next = options.next || ''; },
+    signIn: function(evt){
+        evt.stopPropagation();
+        if (evt.type === 'keypress' && evt.which !== 13) return;
+        var auth = {
+            username: this.ui.username.val().trim(),
+            password: this.ui.password.val().trim(),
+        };
 
-                if (!auth.username){
-                    utils.notifyWindow('Please, enter useraname.');
-                    this.ui.username.focus();
-                    return;
-                }
-                if (!auth.password) {
-                    utils.notifyWindow('Please, enter password.');
-                    this.ui.password.focus();
-                    return;
-                }
+        if (!auth.username){
+            utils.notifyWindow('Please, enter useraname.');
+            this.ui.username.focus();
+            return;
+        }
+        if (!auth.password) {
+            utils.notifyWindow('Please, enter password.');
+            this.ui.password.focus();
+            return;
+        }
 
-                utils.preloader.show();
-                var authModel = new Model.AuthModel(),
-                    view = this;
-                authModel.save(auth, {wait:true})
-                    .always(utils.preloader.hide)
-                    .done(function(){
-                        utils.notifyWindowClose();
-                        view.triggerMethod('action:signin', authModel);
-                    })
-                    .fail(function(xhr){
-                        utils.notifyWindow(xhr.status === 401
-                            ? 'Invalid credentials provided' : xhr);
-                        view.ui.username.focus();
-                    });
-            }
-
-
-        });
-
-        return views;
+        utils.preloader.show();
+        var authModel = new Model.AuthModel(),
+            view = this;
+        authModel.save(auth, {wait:true})
+            .always(utils.preloader.hide)
+            .done(function(){
+                utils.notifyWindowClose();
+                view.triggerMethod('action:signin', authModel);
+            })
+            .fail(function(xhr){
+                utils.notifyWindow(xhr.status === 401
+                    ? 'Invalid credentials provided' : xhr);
+                view.ui.username.focus();
+            });
+    }
 });

7934a6fd267542b6b3c07784918e29bfd267b466
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2016-10-19 18:16:49 +0300

    AC-4860 Add py26 unit test runner
    Change-Id: Ic419c65de59177bfb57d64e5d76f965b0815c204

---
+++ _run_tests26_in_docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+IMAGE='tyzhnenko/centos6-test-base:v1'
+
+docker run -v $(pwd):/appcloud:ro -w /appcloud $IMAGE /bin/bash -c \
+"set -e;
+ echo '###################### Setup requirements ######################';
+ source /venv/bin/activate;
+ pip install -r requirements-dev.txt;
+ echo '######################## Run unit tests ########################';
+ py.test -p no:cacheprovider -v kuberdock-cli"
+ret=$?
+
+exit $ret

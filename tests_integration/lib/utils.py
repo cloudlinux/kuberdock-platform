8da50773e47cc7b211ae47e88f37a129bafd6511
Max Lobur <mlobur@cloudlinux.com>
2016-12-12 17:52:15 +0200

    AC-5428: Integr: stick to older paramiko due to bug in versions 2.X
    Change-Id: I67fc1d1c98037f79e1062ce0f21cfd3321d66ad2

--- tests_integration/lib/utils.py
+++ tests_integration/lib/utils.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import re
 import time
+import sys
 import traceback
 import types
 from itertools import count, islice
@@ -96,9 +97,15 @@ def local_exec_live(cmd, env=None, check_retcode=True):
 
 def ssh_exec(ssh, cmd, timeout=None, check_retcode=True, get_pty=False):
     LOG.debug(u"{}Calling SSH: '{}'{}".format(Style.DIM, cmd, Style.RESET_ALL))
-    _, out, err = ssh.exec_command(cmd, timeout=timeout, get_pty=get_pty)
-    ret_code = out.channel.recv_exit_status()
-    out, err = out.read().strip(), err.read().strip()
+    try:
+        _, out, err = ssh.exec_command(cmd, timeout=timeout, get_pty=get_pty)
+        ret_code = out.channel.recv_exit_status()
+        out, err = out.read().strip(), err.read().strip()
+    except Exception:
+        LOG.debug("Something went wrong in 'ssh_exec':\n{}".format(
+            format_exception(sys.exc_info())
+        ))
+        raise
 
     _proceed_exec_result(out, err, ret_code, check_retcode)
     return ret_code, out, err
@@ -107,12 +114,18 @@ def ssh_exec(ssh, cmd, timeout=None, check_retcode=True, get_pty=False):
 def ssh_exec_live(ssh, cmd, timeout=None, check_retcode=True):
     LOG.debug(u"{}Calling SSH live: '{}'{}".format(Style.DIM, cmd,
                                                    Style.RESET_ALL))
-    interact = SSHClientInteraction(ssh, timeout=timeout, display=True,
-                                    logger=logging.getLogger())
-    interact.expect('.*')
-    interact.send(cmd + "; exit $?")  # needed to not depend on prompt type
-    interact.tail()
-    ret_code = interact.channel.recv_exit_status()
+    try:
+        interact = SSHClientInteraction(ssh, timeout=timeout, display=True,
+                                        logger=logging.getLogger())
+        interact.expect('.*')
+        interact.send(cmd + "; exit $?")  # needed to not depend on prompt type
+        interact.tail()
+        ret_code = interact.channel.recv_exit_status()
+    except Exception:
+        LOG.debug("Something went wrong in 'ssh_exec_live':\n{}".format(
+            format_exception(sys.exc_info())
+        ))
+        raise
 
     _proceed_exec_result("", "", ret_code, check_retcode)
     return ret_code, "", ""
